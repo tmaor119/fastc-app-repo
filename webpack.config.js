dcff2c3d61bf717420e79e8220b3d81f65699dfb
박성서/보안아키텍처팀(YA) <seongseo.park@yanolja.com>
2023-04-02 13:36:07 +0900

    add ver2

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '2048 Game',
+            title: '2048 Game ver2',
         })
     ],
     devServer: {
